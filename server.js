f1ce2fb83586ede5b8a7a7cc5823978f0107e900
Myles Weaver <weaver.mylesh@gmail.com>
2024-01-02 23:23:59 -0500

    add google drive link and fix button formatting for list

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const users = {
 };
 
 const megaLink = process.env.MEGA;
+const googleLink = process.env.GOOGLE;
 
 // Use basic authentication middleware
 app.use('/browse', basicAuth({
@@ -183,8 +184,14 @@ app.get('/mega', async (req,res) => {
     <a  class="glow">
     mCloud
     </a>
-    <a href=${megaLink} class="btn btn-outline-info border-2 w-25" type="submit" style="margin-bottom:6px;">Mega Link</a>
-
+    <ul>
+      <li>
+        <a href=${megaLink} class="btn btn-outline-info border-2" type="submit" style="margin-bottom:6px;">Mega Link</a>
+      </li>
+      <li>
+        <a href=${googleLink} class="btn btn-outline-info border-2" type="submit" style="margin-bottom:6px;">Google Link</a> 
+      </li>
+    </ul>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
   </body>
