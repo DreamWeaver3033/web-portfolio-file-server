e47c7d7da18255bb7b01ad67af1371e1fe292d75
mylesweaver <weaver.mylesh@gmail.com>
2024-02-09 13:30:21 -0500

    fix full image size

--- server.js
+++ server.js
@@ -205,13 +205,13 @@ app.get('/full*', async (req, res) => {
   <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="http://localhost:8080/index.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://mylesweaver.net/index.css">
   </head>
   <body>
-  <div class="image-wrapper">
-      ${listItems}  
-  </div>
+    <div class="image-full">
+        ${listItems}  
+    </div>
   </body>
   </html>
 `);
