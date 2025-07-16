4d720ce1da7b6a94541844e454764db133844bf6
mylesweaver <weaver.mylesh@gmail.com>
2025-07-16 14:18:20 -0400

    remove old links

--- server.js
+++ server.js
@@ -158,13 +158,7 @@ app.get('/cloud', async (req,res) => {
     </a>
     <ul>
       <li>
-        <a href=${megaLink} class="btn btn-outline-info border-2" type="submit" style="margin-bottom:6px;">Mega</a>
-      </li>
-      <li>
         <a href=${googleLink} class="btn btn-outline-info border-2" type="submit" style="margin-bottom:6px;">Google</a> 
-      </li>
-      <li>
-        <a href=${protonLink} class="btn btn-outline-info border-2" type="submit" style="margin-bottom:6px;">Proton</a> 
       </li>
     </ul>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
